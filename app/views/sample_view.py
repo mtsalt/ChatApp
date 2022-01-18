0cedba8f71174a5a53e44f62e98f81c27e72445b
Mt.Salt <juntaro.shioyama@gmail.com>
2022-01-18 21:10:35 +0900

    add login & register page, and routing process

--- app/views/sample_view.py
+++ app/views/sample_view.py
@@ -2,6 +2,18 @@ from flask import render_template
 from . import main
 
 
-@main.route('/')
+@main.route("/", methods=['GET', 'POST'])
 def index():
     return render_template("index.html")
+
+
+@main.route("/register", methods=['GET', 'POST'])
+def register():
+    return render_template("register.html")
+
+
+@main.route("/login", methods=['GET', 'POST'])
+def login():
+    return render_template("login.html")
+
+
