6adacc02ec38f2d368673cf85392f46f0b5c9e56
Mt.Salt <juntaro.shioyama@gmail.com>
2022-01-20 20:59:59 +0900

    add logger sample

--- app/views/auth.py
+++ app/views/auth.py
@@ -1,14 +1,20 @@
 from flask import render_template, request
 from . import main
 
+import logging
+logging.basicConfig(level=logging.DEBUG, filename="./app/log/chatapp.log")
+logger = logging.getLogger(__name__)
+
 
 @main.route("/", methods=['GET', 'POST'])
 def index():
     if request.method == 'GET':
+        logger.info("get index page.")
         # TODO セッションが残っていれば、そのままチャット画面表示
         return render_template("index.html")
 
     if request.method == 'POST':
+        logger.info("post index page.")
         pass
 
 
