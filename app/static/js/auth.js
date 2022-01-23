156329ee37b960de094dcdbb4bc56de3f41f8e13
Mt.Salt <juntaro.shioyama@gmail.com>
2022-01-23 12:54:32 +0900

    add register process (need to implement validation)

--- app/static/js/auth.js
+++ app/static/js/auth.js
@@ -1,16 +1,58 @@
+// PORT
+function postRequest(objData) {
+    let postUrl = location.href;
+    let jsonData = JSON.stringify(objData);
+
+    let xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            // callback process
+            console.log("post success.");
+        }
+    }
+
+    xhr.open("POST", postUrl);
+    xhr.setRequestHeader('Content-type', 'application/json; charset=UTF-8');
+    xhr.withCredentials = true;
+    xhr.send(jsonData);
+}
+
+// REGISTER
+function getRegisterInfo() {
+    let registerInfo = {
+        user_id: document.getElementById("user_id").value,
+        password: document.getElementById("password").value,
+        confirm_password: document.getElementById("confirm_password").value,
+        display_name: document.getElementById("display_name").value,
+        search_id: document.getElementById("search_id").value
+    };
+    return registerInfo;
+}
+
+function execRegister() {
+    let registerInfo = getRegisterInfo();
+    postRequest(registerInfo);
+}
+
+
+// LOGIN
 function getLoginInfo() {
-    let userId = document.getElementById("user_id").value;
-    let password = document.getElementById("password").value;
     let loginInfo = {
-        user_id: userId,
-        password: password
+        user_id: document.getElementById("user_id").value,
+        password: document.getElementById("password").value
     };
     return loginInfo;
 }
 
-function postLoginInfo() {
+function execLogin() {
+    let loginInfo = getLoginInfo();
+    postRequest(loginInfo);
+}
 
-    let postUrl = location.href;
-    let loginInfoJSON = JSON.stringify(getLoginInfo());
+
+// LOGOUT
+// need to get identification info to finish session.
+function execLogout() {
+    let logoutUrl = location.host;
 
 }
--- app/views/auth.py
+++ app/views/auth.py
@@ -1,7 +1,7 @@
 from flask import render_template, request
 from . import main
 from flask import current_app
-
+from app.models.chat_setting.account_manager import create_account
 
 @main.route("/", methods=['GET', 'POST'])
 def index():
@@ -11,7 +11,7 @@ def index():
         return render_template("index.html")
 
     if request.method == 'POST':
-        pass
+        return render_template("success.html")
 
 
 @main.route("/register", methods=['GET', 'POST'])
@@ -21,7 +21,33 @@ def register():
         return render_template("auth/register.html")
 
     if request.method == 'POST':
-        pass
+        # TODO : Validation 処理（必須。クライアントにも実装）
+        user_id = request.json["user_id"]
+        password = request.json["password"]
+        confirm_password = request.json["confirm_password"]
+        display_name = request.json["display_name"]
+        search_id = request.json["search_id"]
+
+        # user_id
+        #    - 同じIDが存在していないか
+        #    - アルファベット + 数字 の文字列で構成されているか
+        #    - 1文字以上 文字以下の文字列で構成されているか
+
+        # password
+        #    - 確認用パスワードと同じ文字列であるか
+        #    - アルファベット + 数字 の文字列で構成されているか
+        #    - 文字以上 文字以下の文字列で構成されているか
+
+        # display_name
+        #    = 文字以上 文字以下の文字列で構成されているか
+
+        # search_id
+        #    - 文字以上 文字以下の文字列で構成されているか
+
+        create_account(user_id, password, display_name, search_id)
+
+        if True:
+            return render_template("auth/login.html")
 
 
 @main.route("/login", methods=['GET', 'POST'])
@@ -31,6 +57,14 @@ def login():
         return render_template("auth/login.html")
 
     if request.method == 'POST':
-        pass
+        user_id = request.json["user_id"]
+        password = request.json["password"]
+        current_app.logger.info("id: " + user_id + ",  pass: " + password)
+        return render_template("success.html")
 
+
+@main.route("/logout", methods=['GET'])
+def logout():
+    # ページアクセスをきっかけにして、セッション情報などからログアウト
+    pass
 
